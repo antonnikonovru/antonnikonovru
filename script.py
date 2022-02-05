573706d5c6c092f5f956fbce31c5da84ebeec7c1
antonnikonovru <97090710+antonnikonovru@users.noreply.github.com>
2022-02-06 02:49:57 +0300

    Rename script.py to scriptmessage.py

--- script.py
+++
@@ -1,3 +0,0 @@
-import win10toast
-object1 = win10toast.ToastNotifier()
-object1.show_toast(title='Title ', msg='"import win10toast object1 = win10toast.ToastNotifier() object1.show_toast(title=' ', msg=' ', duration=10) "', duration=10)
