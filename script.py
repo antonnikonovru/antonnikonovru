6889dd7f727d37581af8c7d3e47303c20a69b98b
antonnikonovru <97090710+antonnikonovru@users.noreply.github.com>
2022-02-06 02:49:28 +0300

    Add files via upload

---
+++ script.py
@@ -0,0 +1,3 @@
+import win10toast
+object1 = win10toast.ToastNotifier()
+object1.show_toast(title='Title ', msg='"import win10toast object1 = win10toast.ToastNotifier() object1.show_toast(title=' ', msg=' ', duration=10) "', duration=10)
